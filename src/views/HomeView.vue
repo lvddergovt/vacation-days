19136bb4249fc57884fbe8f3c60f21a57656e39b
Guido <gbakkes@gmail.com>
2022-03-13 09:41:45 +0100

    Project setup + theme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<button class="btn">Test</button>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Test</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Hoi</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
